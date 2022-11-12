4b1d53350f1e5b3622ce691a26261ae92e994aec
tatekitzhak <tatekosi@gmail.com>
2022-11-12 15:23:40 +0200

    Added a few logic methods of put and delete requests

--- controllers/users.js
+++ controllers/users.js
@@ -15,4 +15,25 @@ export const createUser = (req, res) => {
     users.push({...user, id: uuid() });
     console.log('users:',users)
     res.send("A user added successfully:")
+};
+
+export const getUserByID = (req, res) => {
+    const userID = users.filter( (user) => user.id === req.params.id );
+    res.send(userID)
+};
+
+export const deleteUserByID = (req, res) => {
+    users = users.filter((user) => user.id !== req.params.id );
+
+    res.send("The user deleted successfully!")
+};
+
+export const updateUserByID = (req, res) => {
+    const user = users.find((user) => user.id === req.params.id);
+
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.contact = req.body.contact;
+
+    res.send("The user updated successfully!")
 }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 import express from "express";
 
-import { getUsersList, createUser } from "../controllers/users.js";
+import { getUsersList, createUser, getUserByID, deleteUserByID, updateUserByID } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.get("/users", getUsersList);
+router.get("/user/:id", getUserByID);
 router.post("/user", createUser);
+router.delete("/user/:id", deleteUserByID);
+router.put("/user/:id", updateUserByID);
 
 
 export default router;

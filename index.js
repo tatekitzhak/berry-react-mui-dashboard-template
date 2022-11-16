508f7f6eaecb889d55ac3feb79cef81e31f70ce8
tatekitzhak <tatekosi@gmail.com>
2022-11-16 12:40:03 +0200

    Middleweare and error handling

--- index.js
+++ index.js
@@ -1,58 +1,76 @@
+//https://www.youtube.com/watch?v=AZDTM0DiLG8&list=PLYQSCk-qyTW2ewJ05f_GKHtTIzjynDgjK&index=4&ab_channel=ZachGollwitzer
 import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import EventEmitter from 'events';
-
-
-import userRoutes from "./routes/users.js";
-import tasksRoutes from './routes/tasks.js';
-import axiosAPI from './routes/categories.js';
-
-// Log Events
-import { logEvents } from './logEvents.js';
-
 const app = express();
-const PORT = 5000;
 
-// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
-app.use(bodyParser.json())
-/* 
-allowing below URL to access these APIs end-points
-you can replace this URL(http://localhost:8100) with your
-application URL from where you are calling these APIs
+function middlweare1(req, res, next) {
+    console.log('1st middlweare');
+    const errobj = new Error('I am an error');
+    next(errobj);
+}
+function middlweare(req, res, next) {
+    console.log('1nd middlweare: ');
+    req.customProperty = 100;
+    next();
+}
+function middlweare2(req, res, next) {
+    console.log(`2nd middlweare ${req.customProperty}`);
+    req.customProperty = 500;
+    next();
+}
+// app.use(middlweare1, middlweare2);
+// app.use(errorHandlingMiddlewareFunction);
 
-app.use(cors({origin: 'http://localhost:8100'}));
- */
+const users = [
+    {
+        id: 1,
+        firstName: 'Ran',
+        lastName: 'Itzhak'
+    },
+    {
+        id: 2,
+        firstName: 'Maayan',
+        lastName: 'Itzhak'
+    },
+    {
+        id: 3,
+        firstName: 'Rachale',
+        lastName: 'Itzhak'
+    },
+];
 
-app.use(cors());
-app.use(express.json());
+function loadUser(req, res, next) {
 
-app.use("/", userRoutes);
-app.use('/tasks', tasksRoutes);
-app.use('/axiosAPI', axiosAPI);
+    if (req.params.id) {
+        const user = users.filter(user => user.id == req.params.id);
 
-app.get("/", (req, res) => {
-    res.status(200).json('Hello from node-express-api !')
-    // res.send('Hello from node-express-api !')
+        if (!user.length) {
+            next(new Error("Couldn't find user: "));
+            return;
+        }
+        req.userData = user;
+        next();
+    } else {
+        next(new Error("Couldn't find user: "));
+    }
+    next();
+}
+
+function errorHandlingMiddlewareFunction(error, request, res, next) {
+
+    if (error) {
+        
+        res.send({ errorMessage: error.message })
+    }
+
+    next();
+}
+
+app.get('/user/:id', loadUser, (req, res, next) => {
+    console.log('Get:', req.params, 'userData:',req.userData);
+    res.send({ customProperty: req.userData})
 });
+app.use(errorHandlingMiddlewareFunction);
 
-// If routes not match
-app.all("*", (req, res, next) => {
-    
-    console.log('The request not exist!', next);
-    res.status(404).json('Not Found: ' +404)
-});
-
-// Build log events instance
-class Emitter extends EventEmitter { };
-const appEmitter = new Emitter();
-
-// Add listener for the log event 
-appEmitter.on('log', (msg) => logEvents(msg))
-setTimeout(() => {
-    appEmitter.emit('log', 'Log event emitted.')
-}, 2000);
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port: http://loclalhost:${PORT}`)
+app.listen(3700, (error) => {
+    console.log(`Server is listening on port: http://loclalhost:3700`)
 });
